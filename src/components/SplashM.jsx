2e1ee8cca8de9e31119fc7c3bcfe7a7b87c4c62d
brahmk <brahm.kelly@gmail.com>
2022-08-11 11:18:28 -0400

    about and splash

---
+++ src/components/SplashM.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import "./SplashM.css";
+import splash from "../img/splasher-mb.png";
+import about from "../img/about.png";
+import work from "../img/work.png";
+
+export default function SplashM() {
+  return (
+    <div className="splash-m">
+      <img className="bg" src={splash} />
+      <img className="work" src={work} />
+      <img className="about" src={about} />
+    </div>
+  );
+}
